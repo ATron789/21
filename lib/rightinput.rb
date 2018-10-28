0bedce9b0862960becfc403f93d0ec4a0cfd701a
ATron789 <albertopavano@gmail.com>
2018-10-28 18:42:07 +0000

    refactoring the inputs handling and test automation

--- lib/rightinput.rb
+++ lib/rightinput.rb
@@ -2,10 +2,8 @@ class RightInput
   YES_NO = ['y','n']
   def self.yes_or_no
     choice = gets.chomp.downcase
-    unless YES_NO.include?(choice)
-      puts 'press y to split, press n to keep your hand'
-      return yes_or_no
-    end
-    choice
+    return choice if YES_NO.include?(choice)
+    puts 'press y to split, press n to keep your hand'
+    yes_or_no
   end
 end
